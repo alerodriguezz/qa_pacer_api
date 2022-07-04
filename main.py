22f4630393ce2f1fb209a3ef40da7bd24c17326c
arod <alexrodriguez218@yahoo.com>
2022-07-04 02:41:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+import os 
+import json 
+
+my_login = os.environ['user']
+my_pass = os.environ['pass']
+
+
+#Authentification
+authUrl="https://qa-login.uscourts.gov/services/cso-auth"
+authHeaders={'Content-type': 'application/json',
+'Accept': 'application/json'}
+authBody={
+  "loginId": my_login,
+"password": my_pass,
+"clientCode": "testclientcode",
+"redactFlag": "1"
+}
+
+authResponse = requests.post(authUrl,json=authBody,headers=authHeaders)
+
+
+print("Authentication response: ",authResponse.json(),"\n")
+
+myToken = authResponse.json()['nextGenCSO']
+
+#save token 
+
+print ("token: ",myToken,"\n")
+
+#Batch Party Search
+
+batchHeaders={'Content-type': 'application/json',
+'Accept': 'application/json','X-NEXT-GEN-CSO': myToken }
+batchBody={
+"federalBankruptcyChapter" : [ 11 ],
+"dateFiledFrom": "2016-01-01",
+"dateFiledTo":"2016-01-31", 
+}
+
+batchUrl="https://qa-pcl.uscourts.gov/pcl-public-api/rest/cases/find"
+
+batchResponse = requests.post(batchUrl,json=batchBody,headers=batchHeaders)
+
+obj=batchResponse.json()
+
+print("Search response: ",json.dumps(obj,indent=4),"\n")
+
+
+#logout 
+url="https://qa-login.uscourts.gov/services/cso-logout"
+authHeaders={'Content-type': 'application/json',
+'Accept': 'application/json'}
+authBody={
+  "nextGenCSO": myToken,
+}
+
+
+logoutResponse = requests.post(url,json=authBody,headers=authHeaders)
+
+print ("Logout response",logoutResponse.json())
+
