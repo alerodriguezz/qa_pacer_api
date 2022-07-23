933ae1fbf15126907ebe5ea5e07780b45de6af67
arod <alexrodriguez218@yahoo.com>
2022-07-23 21:56:23 +0000

    added headers and other built in mechanize functions to avpid bot detection

--- main.py
+++ main.py
@@ -62,11 +62,19 @@ for i in obj['content']:
     link = i['caseLink'].replace("iqquerymenu","qryParties")
     print("Link:", link,"\n")
 
+
+
+
+  
     ssl._create_default_https_context = ssl._create_unverified_context
     browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.set_handle_refresh(False)
+    browser.addheaders = [('user-agent', '   Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3'),
+('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
     browser.open(link)
     for f in browser.forms():
-        print (f.name, "\")
+        print (f.name, "\n")
     #browser.select_form(nr = 0)
     #browser.form['username'] = my_login
     #browser.form['password'] = my_pass
