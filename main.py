00191429bd32195412a8c37604ef7451f3d14e5a
arod <alexrodriguez218@yahoo.com>
2022-07-26 01:51:08 +0000

    added code that converts extracted data into a csv file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import json 
 import mechanicalsoup
 import warnings
-import pandas
+import pandas as pd
 from commonregex import CommonRegex
 import re
 
@@ -78,15 +78,14 @@ br["loginForm:password"]= my_pass
 br.submit_selected()
 
 data={
-  'Name/Business':[],
+  'Name':[],
   'Address':[],
-  'Phone Number':[],
-  'SSN':[],
+  'Phone Number':[]
+}
+"""'SSN':[],
   'Bankruptcy Chapter': [],
   'Date Filed' : [],
-  'Case Id' : []
-}
-
+  'Case Id' : []"""
 for i in obj['content']:
   if "caseLink" in i.keys():
     print("Court ID: ", i['courtId'])
@@ -114,45 +113,67 @@ for i in obj['content']:
     print(name)
     if (name=="Box Check"):
       print ("\n---------------------------------------------\n")
-      continue
+      pass
+    else:
+    
+
+      #remove name from paragraph string obj
+      paragraph = br.get_current_page().find("p",id="3").text.replace(name,"")
       
-    
+      #retrieve debtor address w/ regex
+      address_validate_pattern = r"(?:(\d+ [A-Za-z][A-Za-z ]+)[\s,]*([A-Za-z#0-9][A-Za-z#0-9 ]+)?[\s,]*)?(?:([A-Za-z][A-Za-z ]+)[\s,]+)?((?=AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA‌​|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD‌​|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)[A-Z]{2})(?:[,\s]+(\d{5}(?:-\d{4})?))?"
+      temp=re.search(address_validate_pattern, paragraph) 
+      address=temp.group()
+      print (address)
+  
+      #remove address from paragraph string obj
+      paragraph = paragraph.replace(address,"")
+  
+      #retrieve phone number 
+      parser= CommonRegex(paragraph)
+      phone_num=parser.phones[:1]
+      print("phone number: ",str(phone_num)[2:-2])
+  
+      #remove phone num from paragraph string obj
+      paragraph = paragraph.replace(str(phone_num)[2:-2],"")
+      
+  
+      ssn_or_tax_num=paragraph.split(' ')[0]
+      temp_num=paragraph.split(' ')[-1]
+  
+      print ('\n',ssn_or_tax_num,": ", temp_num)
+      print ("\n---------------------------------------------\n")
 
-    #remove name from paragraph string obj
-    paragraph = br.get_current_page().find("p",id="3").text.replace(name,"")
-    
-    #retrieve debtor address w/ regex
-    street_address_validate_pattern = r"(?:((?:\d[\d ]+)?[A-Za-z][A-Za-z ]+)[\s,]*([A-Za-z#0-9][A-Za-z#0-9 ]+)?[\s,]*)?(?:([A-Za-z][A-Za-z ]+)[\s,]+)?((?=AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA‌​|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD‌​|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)[A-Z]{2})(?:[,\s]+(\d{5}(?:-\d{4})?))?"
-    temp=re.search(street_address_validate_pattern, paragraph) 
-    address=temp.group()
-    print (address)
+#address= 
 
-    #remove address from paragraph string obj
-    paragraph = paragraph.replace(address,"")
 
-    #retrieve phone number 
-    parser= CommonRegex(paragraph)
-    phone_num=parser.phones[:1]
-    print("phone number: ",str(phone_num)[2:-2])
+    if name:
+      data['Name'].append(name)
+    else:
+      data['Name'].append("n/a")
+    if address:
+      data['Address'].append(address)
+    else:
+      data['Address'].append("n/a")
+    if phone_num:
+      data['Phone Number'].append(str(phone_num)[2:-2])
+    else:
+      data['Phone Number'].append("n/a")
+print(data)
+table = pd.DataFrame(data)
+#table = pd.DataFrame.from_dict(data)
 
-    #remove phone num from paragraph string obj
-    paragraph = paragraph.replace(str(phone_num)[2:-2],"")
-    
+table.index+= 1
+print(table)
 
-    ssn_or_tax_num=paragraph.split(' ')[0]
-    temp_num=paragraph.split(' ')[-1]
-
-    print ('\n',ssn_or_tax_num,": ", temp_num)
-
-    """
-    parser= CommonRegex()
-    print (parser.addresses(paragraph))
-    """
+table.to_csv('debtors.csv',sep=',',encoding='utf-8')
+"""
+parser= CommonRegex()
+print (parser.addresses(paragraph))
+"""
    
     
-    print ("\n---------------------------------------------\n")
 
-    #address= 
 
 
 
